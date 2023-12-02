0d2275382b39eae17455aeb00d350c862286afc6
Akachukwu Egboluche <egbolucheakachukwu@gmail.com>
2023-12-02 12:32:28 +0100

    changes

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -61,7 +61,7 @@ export const useGetPosts = () => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
     queryFn: getInfinitePosts,
-    getNextPageParam: (lastPage: { documents: string | any[]; }) => {
+    getNextPageParam: (lastPage) => {
       if (lastPage && lastPage.documents.length === 0) return null;
 
       const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id;
